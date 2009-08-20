916bd01ac52a2c17490a0da6c71b7dad635b6a75
Alan Hogan <contact@alanhogan.com>
2009-08-20 16:19:34 -0700

    Initial Checkin

---
+++ jquery.wordcount.js
@@ -0,0 +1,39 @@
+/*
+ * 
+ * Textarea Word Count Jquery Plugin 
+ * Version 1.0
+ * 
+ * Copyright (c) 2008 Roshan Bhattarai
+ * website : http://roshanbh.com.np
+ * 
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+*/
+
+jQuery.fn.wordCount = function(params){
+	var p = {
+		counterElement:"display_count"
+	};
+	var total_words;
+	
+	if(params) {
+		jQuery.extend(p, params);
+	}
+	
+	//for each keypress function on text areas
+	this.keypress(function()
+	{ 
+		total_words=this.value.split(/[\s\.\?]+/).length;
+		jQuery('#'+p.counterElement).html(total_words);
+	});	
+	this.blur(function()
+	{
+		jQuery(this).keypress();
+	});
+	this.focus(function()
+	{
+		jQuery(this).keypress();
+	});
+};
